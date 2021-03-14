e1a08fce2974b3a0abc183f52de8aea5bc0e63e0
Panadestein <rpana92@gmail.com>
2021-03-14 12:06:59 +0100

    Fixed layout 3 pages

--- auto/cv_rpanades.el
+++ auto/cv_rpanades.el
@@ -4,7 +4,7 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("moderncv" "10pt" "a4paper" "sans")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("geometry" "scale=0.87")))
+                     '(("inputenc" "utf8") ("geometry" "scale=0.75")))
    (TeX-run-style-hooks
     "latex2e"
     "moderncv"
